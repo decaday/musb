6f5322a75a5e03fd1d87da4ad2f8976cc63d2f6a
Decaday <myDecaday@outlook.com>
2026-07-23 18:45:45 +0800

    Refactor codegen

--- build.rs
+++ build.rs
@@ -1,15 +1,13 @@
 #[cfg(not(feature = "prebuild"))]
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 #[cfg(not(feature = "prebuild"))]
 use std::{env, fs, path::Path};
-#[cfg(not(feature = "prebuild"))]
-use serde_yaml;
 
 mod build_src;
 use build_src::feature::*;
 
 #[cfg(not(feature = "prebuild"))]
-use build_src::{block::*, build_serde::*, fieldset::*, gen, profile::*};
+use build_src::{block::*, build_serde::*, gen, profile::*};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rerun-if-changed=registers");
@@ -42,46 +40,40 @@ fn build() {
 
     // 1. Load the block and process any inheritance to get the final, merged block.
     //    (This part remains unchanged from the previous modification)
-    let final_block = load_and_merge_block(&profile.block);
+    let mut final_block = load_and_merge_block(&profile.block);
 
-    // 2. Serialize the MODIFIED `Value` object. Numerical values will be unquoted.
-    let yaml_content = serialize_block_to_yaml_string(&final_block);
-
-    // The rest of the function continues as before, writing the modified YAML content to a file.
+    let mut added_paths = HashSet::new();
+    let mut regs_yaml_files = Vec::new();
     let out_dir = env::var("OUT_DIR").unwrap();
     let merged_block_path = Path::new(&out_dir).join(format!("{}_merged.yaml", &profile.block));
-    fs::write(&merged_block_path, yaml_content).unwrap();
-
-    // Extract fieldsets from the merged block object we already have in memory.
-    let fieldsets = extract_fieldsets_from_block(&final_block);
-
-    let fieldset_db = FieldsetDatabase::new_from_file();
-
-    let mut regs_yaml_files = Vec::new();
 
     // Add the path to our NEWLY CREATED merged block file to the list of files to concatenate.
     regs_yaml_files.push(merged_block_path.to_str().unwrap().to_string());
 
-    for fieldset in &fieldsets {
-        let version = if let Some(patch) = profile.patches.iter().find(|p| p.fieldset == *fieldset)
+    for item in &mut final_block.items {
+        let target_path = if let Some(patch) = profile.patches.iter().find(|p| p.item == item.name)
         {
-            patch.version.clone()
+            patch.path.clone()
         } else {
-            "std".to_string()
+            item.fieldset.clone()
         };
 
-        let mode = "peri".to_string();
+        let full_path = Path::new("registers/fieldsets").join(&target_path);
 
-        let path = fieldset_db.find_files(
-            fieldset,
-            &Some(HashSet::from([version.clone()])),
-            &Some(HashSet::from(["host".to_string()])),
-            &Some(HashSet::from([mode.clone()])),
-        );
+        if added_paths.insert(target_path.clone()) {
+            println!("{} -> {}", item.name, target_path);
+            regs_yaml_files.push(full_path.to_str().unwrap().to_string());
+        }
 
-        println!("{} {} {}", fieldset, version, &path);
-        regs_yaml_files.push(path);
+        let content = fs::read_to_string(&full_path).unwrap();
+        let fieldset_line = content.lines().find(|l| l.starts_with("fieldset/")).unwrap();
+        let logical_name = fieldset_line.trim_start_matches("fieldset/").trim_end_matches(':').to_string();
+        item.fieldset = logical_name;
     }
+
+    // 2. Serialize the MODIFIED `Value` object. Numerical values will be unquoted.
+    let yaml_content = serialize_block_to_yaml_string(&final_block);
+    fs::write(&merged_block_path, yaml_content).unwrap();
 
     let features = FeatureGenerator::get_from_profile(&profile);
     features.gen();
